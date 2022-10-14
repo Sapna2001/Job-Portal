a045ca54a7ed482ab41fe4760307b6386e6cea54
Sapna2001 <harisapnanair@gmail.com>
2022-10-14 19:29:17 +0530

    set up

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const dbConnect = require("./dbConnect");
+const port = process.env.PORT || 5000;
+const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
+}
+
+app.get("/", (req, res) => res.send("Job Portal"));
+app.listen(port, () => console.log(`Job Portal app listening on port ${port}!`));

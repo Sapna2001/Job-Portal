ba7919f5d18b50caa694259ffb24bb2ad9cc3b3b
Sapna2001 <harisapnanair@gmail.com>
2022-10-26 22:02:45 +0530

    loader

--- client/src/App.js
+++ client/src/App.js
@@ -14,25 +14,42 @@ import Referral from "./Pages/Referral";
 import Resume from "./Pages/Resume";
 import Company from "./Pages/Company";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getAllJobs } from "./redux/actions/jobsActions";
+import { css } from "@emotion/react";
+import RingLoader from "react-spinners/RingLoader";
 
 function App() {
+  const { loader } = useSelector((state) => state.loaderReducer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllJobs());
+  }, []);
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/appliedjobs" element={<AppliedJobs />} />
-          <Route path="/postjob" element={<PostJob />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/jobs/:id" element={<JobInfo />} />
-          <Route path="/posted" element={<PostedJobs />} />
-          <Route path="/editjob/:id" element={<EditJob />} />
-          <Route path="/users/:id" element={<UserInfo />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="/referral" element={<Referral />} />
-          <Route path="/company" element={<Company/>} />
-        </Routes>
-      </BrowserRouter>
+      <div className="App">
+        {loader && (
+          <div className="sweet-loading text-center">
+            <RingLoader color={"#001529"} />
+          </div>
+        )}
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/appliedjobs" element={<AppliedJobs />} />
+            <Route path="/postjob" element={<PostJob />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/jobs/:id" element={<JobInfo />} />
+            <Route path="/posted" element={<PostedJobs />} />
+            <Route path="/editjob/:id" element={<EditJob />} />
+            <Route path="/users/:id" element={<UserInfo />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/referral" element={<Referral />} />
+            <Route path="/company" element={<Company />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
   );
 }

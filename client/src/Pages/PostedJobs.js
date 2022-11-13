139d36525180bea45495d376e9e6697d8e751945
Sapna2001 <harisapnanair@gmail.com>
2022-11-13 18:06:03 +0530

    posted jobs page

--- client/src/Pages/PostedJobs.js
+++ client/src/Pages/PostedJobs.js
@@ -1,14 +1,54 @@
-import React from 'react'
-import DefaultLayout from '../Components/DefaultLayout'
+import React from "react";
+import DefaultLayout from "../Components/DefaultLayout";
+import { useSelector } from "react-redux";
+import { Table } from "antd";
+import moment from "moment";
 
-function Home() {
+function PostedJobs() {
+  const allJobs = useSelector((state) => state.jobsReducer).jobs;
+  const userId = JSON.parse(localStorage.getItem("user"))._id;
+  const userPostedJobs = allJobs.filter((job) => job.postedBy === userId);
+
+  const columns = [
+    {
+      title: "Title",
+      dataIndex: "title",
+    },
+    {
+      title: "Company",
+      dataIndex: "company",
+    },
+    {
+      title: "Posted On",
+      dataIndex: "postedOn",
+    },
+    {
+      title: "Applied Candidates",
+      dataIndex: "appliedCandidates",
+    },
+  ];
+
+  const dataSource = [];
+
+  for (var job of userPostedJobs) {
+    var obj = {
+      title: job.title,
+      company: job.company,
+      postedOn: moment(job.createdAt).format("MMM DD yyyy"),
+      appliedCandidates: job.appliedCandidates.length,
+      completeJobData: job,
+    };
+    dataSource.push(obj);
+  }
+
   return (
     <div>
-    <DefaultLayout>
+      <DefaultLayout>
         <h1>Posted Jobs</h1>
-    </DefaultLayout>
+        <Table columns={columns} dataSource={dataSource} />
+      </DefaultLayout>
     </div>
-  )
+  );
 }
 
-export default Home
+export default PostedJobs;

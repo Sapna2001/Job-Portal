3b5de9f9ceb68bb860d544b98b4c3e828dfdbecd
Sapna2001 <harisapnanair@gmail.com>
2022-11-26 17:59:50 +0530

    user info

--- client/src/Pages/UserInfo.js
+++ client/src/Pages/UserInfo.js
@@ -1,14 +1,81 @@
-import React from 'react'
-import DefaultLayout from '../Components/DefaultLayout'
+import React from "react";
+import DefaultLayout from "../Components/DefaultLayout";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import "../Resources/stylesheets/userInfo.css"
 
 function Home() {
+  const { users } = useSelector((state) => state.usersReducer);
+  const params = useParams();
+  const user = users.find((user) => user._id === params.id);
+
   return (
     <div>
-    <DefaultLayout>
-        <h1>User Info</h1>
-    </DefaultLayout>
+      <DefaultLayout>
+        {user && (
+          <div className = "p-5">
+            <h2>
+              <b>Personal inforamtion</b>
+            </h2>
+            <p>
+              <b>First name : </b>
+              {user.firstName}
+            </p>
+            <p>
+              <b>Last name : </b>
+              {user.lastName}
+            </p>
+            <p>
+              <b>Email : </b>
+              {user.email}
+            </p>
+            <p>
+              <b>Mobile Number : </b>
+              {user.mobileNumber}
+            </p>
+            <p>
+              <b>Address : </b>
+              {user.address}
+            </p>
+
+            <hr className = "space" />
+
+            <h3>
+              <b>Skills</b>
+            </h3>
+
+            {user.skills.map((skill) => {
+              return <li>{skill}</li>;
+            })}
+
+            <hr className = "space" />
+            <h3>
+              <b>Education</b>
+            </h3>
+            {user.education.map((education) => {
+              return <li>{education}</li>;
+            })}
+            <hr className = "space" />
+
+            <h3>
+              <b>Projects</b>
+            </h3>
+            {user.projects.map((project) => {
+              return <li>{project}</li>;
+            })}
+
+            <hr className = "space" />
+            <h3>
+              <b>Experience</b>
+            </h3>
+            {user.experience.map((experience) => {
+              return <li>{experience}</li>;
+            })}
+          </div>
+        )}
+      </DefaultLayout>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
